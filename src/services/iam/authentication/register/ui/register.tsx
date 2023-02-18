e1ac2412a9e4aaa595a6d2e42a5d2b5bc3e97673
akushniruk <akushniruk@openware.com>
2023-02-18 14:15:31 +0100

    feat: added input component + shared styles

--- src/services/iam/authentication/register/ui/register.tsx
+++ src/services/iam/authentication/register/ui/register.tsx
@@ -1,16 +1,23 @@
-import React, { FC, useCallback } from "react";
-import { View, Text, Button, TextInput } from "react-native";
+import React, { FC, useCallback, useMemo } from "react";
+import { View, Button } from "react-native";
 import { useRegisterUserMutation } from "../api/registerApi";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import {
+    Controller,
+    SubmitHandler,
+    UseControllerReturn,
+    useForm,
+} from "react-hook-form";
 import { RegisterType, RegisterResolver, registerSchema } from "../lib/schema";
+import Input from "../../../../../shared/ui/input";
 
 const Register: FC = () => {
-    const inputFields = registerSchema.keyof()._def.values;
+    const inputFields: string[] = registerSchema.keyof()._def.values;
     const {
         control,
         handleSubmit,
         formState: { errors },
     } = useForm({
+        mode: "onBlur",
         resolver: RegisterResolver,
     });
 
@@ -24,46 +31,70 @@ const Register: FC = () => {
         registerUser(formData);
     };
 
+    console.log(errors);
+
+    const renderInput = useCallback(
+        ({ field }: UseControllerReturn) => (
+            <Input
+                onBlur={field.onBlur}
+                onChangeText={field.onChange}
+                value={field.value}
+                placeholder={field.name}
+                label={field.name}
+                testID={field.name}
+                keyboardType={
+                    field.name === "email" ? "email-address" : "default"
+                }
+                secureTextEntry={field.name === "password"}
+            />
+        ),
+        [inputFields, control]
+    );
+
+    const renderRegisterForm = useMemo(() => {
+        return inputFields.map((name: string) => {
+            return (
+                <Controller
+                    key={name}
+                    control={control}
+                    rules={{ required: true }}
+                    name={name}
+                    render={renderInput}
+                />
+            );
+        });
+    }, [inputFields, control]);
+
     return (
         <View>
-            <Controller
-                control={control}
-                rules={{
-                    required: true,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                        onBlur={onBlur}
-                        onChangeText={onChange}
-                        value={value}
-                        placeholder="email"
-                    />
-                )}
-                name="email"
-            />
-            {errors.email && <Text>This is required. Email</Text>}
+            {renderRegisterForm}
+            {/* {errors.email && <Text>This is required. Email</Text>} */}
 
-            <Controller
-                control={control}
-                rules={{
-                    maxLength: 100,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                        onBlur={onBlur}
-                        onChangeText={onChange}
-                        value={value}
-                        placeholder="password"
-                    />
-                )}
-                name="password"
+            <Button
+                disabled={!errors}
+                title="Submit"
+                onPress={handleSubmit(onSubmitHandler)}
             />
-            {errors.password && <Text>This is required. passworf</Text>}
-
-            {/* @ts-ignore */}
-            <Button title="Submit" onPress={handleSubmit(onSubmitHandler)} />
         </View>
     );
 };
 
 export default Register;
+
+{
+    /* <Controller
+control={control}
+rules={{
+    required: true,
+}}
+render={({ field: { onChange, onBlur, value } }) => (
+    <Input
+        onBlur={onBlur}
+        onChangeText={onChange}
+        value={value}
+        placeholder={name}
+    />
+)}
+name={name}
+/>; */
+}
