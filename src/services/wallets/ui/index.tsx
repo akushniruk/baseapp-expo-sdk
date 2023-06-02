4724d08f8ed2088dfaee193ad05c20f0aff43a20
akushniruk <akushniruk@openware.com>
2023-06-02 12:59:23 +0200

    feat: connect wallets api to ui

--- src/services/wallets/ui/index.tsx
+++ src/services/wallets/ui/index.tsx
@@ -1,18 +1,57 @@
 import { useLinkTo } from "@react-navigation/native";
-import React, { FC, useMemo } from "react";
-import { View, Text, Pressable } from "react-native";
+import React, { FC, useCallback, useMemo } from "react";
+import { View, Text, Pressable, VirtualizedList } from "react-native";
 import { ArrowRightIcon } from "../../../assets/profile";
 import { useThemeContext } from "../../../shared/hooks/useThemeContext";
 import { walletsStyles } from "./wallets.style";
+import { IWallet } from "../api/types";
+import { useAppSelector } from "../../../shared";
+import { RootState } from "../../../shared/providers/redux/model/store";
 
-export const Wallets: FC = () => {
-    const linkTo = useLinkTo();
+interface IWallets {
+    navigation?: any;
+}
+
+export const Wallets: FC<IWallets> = ({ navigation }: IWallets) => {
     const { theme } = useThemeContext();
     const styles = useMemo(() => walletsStyles(theme), [theme]);
 
+    const wallets: IWallet[] | null = useAppSelector((state: RootState) => state.wallets.list);
+
+    const renderWalletRow = useCallback((wallet: IWallet) => {
+        return (
+            <Pressable
+                key={`${wallet.currency}`}
+                // style={styles.row}
+                onPress={() => handleUpdateCurrentMarket(wallet)}
+            >
+                <Text>{wallet.name}</Text>
+            </Pressable>
+        );
+    }, []);
+
+    const getItemCount = (_data: unknown) => wallets?.length;
+
+    const getItem = (_data: unknown, index: number) => {
+        return wallets[index];
+    };
+
+    const handleUpdateCurrentMarket = (wallet: IWallet) => {
+        // TODO: setup navigation for deposit and withdrawal
+        navigation?.navigate("Deposit", { id: wallet?.currency });
+    };
+
     return (
-        <View style={{ height: "100%" }}>
-            <Text>Wallets</Text>
+        <View>
+            <VirtualizedList
+                initialNumToRender={14}
+                renderItem={({ item }) => renderWalletRow(item)}
+                keyExtractor={(item) => item.currency}
+                getItemCount={getItemCount}
+                getItem={getItem}
+                maxToRenderPerBatch={14}
+                // style={styles.bodyContainer}
+            />
         </View>
     );
 };
